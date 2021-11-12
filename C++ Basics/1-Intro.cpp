5f82ee43c36a13287fff3987e78692924979bf10
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-11-12 14:58:26 +0530

    Visual Studio Changes : master

---
+++ C++ Basics/1-Intro.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+using namespace std;
+
+int main(){
+	//* Single Line Comment.
+	/*
+	* Multi 
+	* Line 
+	* Comment
+	*/
+
+	cout << "Hello World" << endl;
+	printf("Hello World \n");
+
+	int a;
+	float b;
+	char c;
+	double d;
+	string s;
+
+	a = 20;
+	b = 20.0012;
+	c = 's';
+	d = 20.10012910;
+	s = "shantanu";
+
+	cout << s << endl << s[2] << endl;
+	printf("%d \n%f \n%c \n%lf", a, b, c, d);
+
+	return 0;
+}
